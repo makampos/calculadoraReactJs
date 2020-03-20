462b1082c1e7930a066ec272357545f9f48b5cac
Matheus de Campos <matheus.campos@gelt.com.br>
2020-03-20 10:18:14 -0300

    implementação do método para realizar as operações

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,7 +27,29 @@ clearMemory(){
 }
 
 setOperation(operation){
-    console.log(operation)
+    if (this.state.current === 0) {
+        this.setState ({ operation, current: 1, clearDisplay: true})
+    } else {
+        const equals = operation ==='='
+        const currentOperation = this.state.operation
+        const values = {...this.state.values}
+
+        try {
+            values[0] = eval (`${values[0]} ${currentOperation} ${values[1]}`)
+        } catch(e) {
+            values[0] = this.state.values[0]
+        }
+        
+        values[1] = 0
+        
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals, 
+            values
+        })
+    }
 }
 
 addDigit(n) {
@@ -41,7 +63,7 @@ addDigit(n) {
      const displayValue = currentValue + n
         this.setState({ displayValue, clearDisplay: false})
      
-    if ( n !== ".") {
+    if ( n !== '.') {
         const i = this.state.current
         const newValue = parseFloat(displayValue)
         const values = [...this.state.values]
@@ -55,7 +77,7 @@ addDigit(n) {
             <div className="calculator">
                 <Display value={this.state.displayValue}/>               
                 <Button label="AC" click={this.clearMemory} />
-                <Button label="/"click={this.setOperation} />
+                <Button label="/"click={this.setOperation}  />
                 <Button label="7"click={this.addDigit}/>
                 <Button label="8"click={this.addDigit}  />
                 <Button label="9"click={this.addDigit}/>
@@ -68,7 +90,7 @@ addDigit(n) {
                 <Button label="2"click={this.addDigit}/>
                 <Button label="3"click={this.addDigit}/>
                 <Button label="+"click={this.setOperation}/>
-                <Button label="0"click={this.addDigit} double/>
+                <Button label="0"click={this.addDigit} />
                 <Button label="."click={this.addDigit}/>
                 <Button label="="click={this.setOperation}/>
             </div>
